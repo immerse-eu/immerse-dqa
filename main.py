ffa3f3596c4df55bc8ffe8b149db8c69edb56c43
thaaanks <alleswirdbesser3@gmail.com>
2023-09-01 19:02:28 +0200

    added date to filename

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ import yaml
 import pandas as pd
 from tabulate import tabulate
 from functools import reduce
+from datetime import datetime
 
 # Required modules
 import prepare_dashboard
 import prepare_dataquier
 import dashboard_visualization_maganamed
-import dashboard_visualization_movisensxs
+import dashboard_visualization_movisensxs02
 
 # Required modules
 import maganamed
+
+# Preparation for file naming
+today = datetime.today()
+created_date = today.strftime('%Y-%m-%d')
 
 # Read configuration file
 with open("config.yaml", "r") as f:
@@ -27,8 +32,8 @@ dfMaganamed = maganamed.compileMaganamedData(config)
 dfDashboard = prepare_dashboard.prepareDashboard(config, dfMaganamed)
 
 # Export merged dataframe to CSV file
-dfDashboard.to_csv(config["localPaths"]["basePathDqa"] + "/dashboard.csv", sep = ";", index = False)
+dfDashboard.to_csv(config["localPaths"]["basePathDqa"] + "/maganamed_dashboard" + created_date + ".csv", sep = ";", index = False)
 
 # generate dashboard figures
 dashboard_visualization_maganamed.prepareFigures(config, dfDashboard)
-dashboard_visualization_movisensxs.prepareFigures(config)
+dashboard_visualization_movisensxs02.prepareFigures(config)
